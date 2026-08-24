9bea246f39ca8f70510b3713b8a45ea34bd8c303
logzati01 <logzati01@gmail.com>
2026-08-24 17:16:21 +0100

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from 'tailwindcss'
+const config: Config = {
+  content: [ './src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}' ],
+  theme: { extend: { colors: { primary: '#092C74', secondary: '#FF9B00' }, fontFamily: { sans: ['var(--font-tajawal)', 'sans-serif'] } } },
+  plugins: [],
+}
+export default config
